9b2f19d5304f6ef57480cb89debf515f8cb5a3f6
“Tejashri <tpancha1@uncc.edu>
2025-08-07 11:54:31 -0400

    feat: add all CRUD endpoints and complete final setup

--- FinalProject/api/main.py
+++ FinalProject/api/main.py
@@ -1,15 +1,13 @@
 import uvicorn
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import index as indexRoute
 from .models import model_loader
+from .routers import index as indexRoute  # OR .controllers.index if you move it
 from .dependencies.config import conf
-
 
 app = FastAPI()
 
 origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,9 +16,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Load models and routes
 model_loader.index()
 indexRoute.load_routes(app)
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host=conf.app_host, port=conf.app_port)
